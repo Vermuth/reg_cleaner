ed40776cbe84c5c16fa64a9432aed9d242c1dd1a
Damir Makhmutov <damir.makhmutov@headmade.pro>
2016-09-08 17:30:17 +0300

    Mark images as deleted only for needed repo

--- lib/clean.rb
+++ lib/clean.rb
@@ -16,10 +16,8 @@ auth = {
 repos_array = HTTParty.get("https://#{registry_host}/v2/_catalog", basic_auth: auth, verify: false).parsed_response["repositories"]
 repos = repos_array.to_s.gsub(/\[|\]/, "").gsub(/"/, "")
 
-p repos
-
 puts ""
-puts "We will be clean all untagged images"
+puts "We will clean all untagged images"
 puts ""
 puts "Please ssh to #{registry_host} and run the following command as root: "
 puts ""
@@ -33,26 +31,26 @@ array = dry_run_output.split("\n")
 $/ = "\n"
 
 # Begin sanitizing the data by pulling out only lines with specified repo name
-stdin_array = []
+repo_stdin_array = []
 array.each do |line|
   repos.each do |repo_name|
     if line.include?("#{repo_name}: marking manifest")
-      stdin_array << line
+      repo_stdin_array << [repo_name, line]
     end
   end
 end
 
 # Pull out only the SHA's of images
-sha_array = []
-stdin_array.each do |manifest|
+repo_name_sha_array = []
+repo_stdin_array.each do |repo_name, manifest|
   sha = manifest.split("manifest ").last
-  sha_array << sha
+  repo_name_sha_array << [repo_name, sha]
 end
 
 puts ""
 puts "These are the SHAs of every #{repos.join(', ')} image on the registry"
 puts ""
-puts sha_array
+puts repo_name_sha_array
 puts ""
 
 # Find the SHA for the image currently labeled as latest. We want to keep this image
@@ -61,26 +59,28 @@ repos.each do |repo_name|
   lates_sha_array << HTTParty.get("https://#{registry_host}/v2/#{repo_name}/manifests/latest", basic_auth: auth, verify: false, headers: {"Accept" => "application/vnd.docker.distribution.manifest.v2+json"}).headers["docker-content-digest"]
 end
 
-# Remove the latest SHA from the array of SHA's
+# Remove array with latest SHA from the array of report_names and SHA's
 lates_sha_array.each do |latest_sha|
-  sha_array.delete(latest_sha)
+  repo_name_sha_array.each do |repo_sha|
+    repo_name_sha_array.delete(repo_sha) if repo_sha.include?(latest_sha)
+  end
 end
+
 puts ""
 puts "This is the list of SHAs of images to be deleted:"
-puts sha_array
+puts repo_name_sha_array
 puts ""
 puts "We'll now use the v2 Docker Registry API to mark these images for deletion"
 puts ""
 
 # Mark images for deletion
-sha_array.each do |sha|
-  repos.each do |repo_name|
-    response = HTTParty.delete("https://#{registry_host}/v2/#{repo_name}/manifests/#{sha}", basic_auth: auth, verify: false)
-    if response.code == 202
-      puts "#{sha} marked for deletion"
-    else
-      puts response.code
-    end
+repo_name_sha_array.each do |repo_name, sha|
+  response = HTTParty.delete("https://#{registry_host}/v2/#{repo_name}/manifests/#{sha}", basic_auth: auth, verify: false)
+
+  if response.code == 202
+    puts "#{repo_name}: #{sha} marked for deletion"
+  else
+    puts "something wrong for #{repo_name}, response: #{response.code}"
   end
 end
 
